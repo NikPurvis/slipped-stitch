ac7683b2852ee1d61b718b8bf5bc41f44585d1a9
NikPurvis <nikpurvis@gmail.com>
2022-05-23 20:48:42 -0600

    Created show comment route with full author info

--- ss-api/app/routes/comment_routes.js
+++ ss-api/app/routes/comment_routes.js
@@ -27,6 +27,30 @@ const router = express.Router()
 // Begin Routes
 /////////////////////
 // 
+// SHOW
+// GET - retrieve a comment
+router.get("/projects/:projectId/:commentId", (req, res, next) => {
+    const projectId = req.params.projectId
+    const commentId = req.params.commentId
+    Project.findOne(
+        { "_id": ObjectId(projectId) },
+        { "comments": {
+            $elemMatch: { _id: ObjectId(commentId) }}
+        }
+    )
+    .populate({
+        path: "comments",
+        populate: {
+            path: "owner",
+            model: "User"
+        }
+    })
+    .then(handle404)
+    .then(project => res.status(200).json({
+        project: project.toObject() }))
+    .catch(next)
+})
+
 // CREATE
 // POST - create a comment
 router.post("/projects/:projectId/newcomment", requireToken, removeBlanks, (req, res, next) => {
@@ -44,6 +68,62 @@ router.post("/projects/:projectId/newcomment", requireToken, removeBlanks, (req,
         })
         .then(project => res.status(201).json({ project: project }))
         .catch(next)    
+})
+
+// UPDATE
+// PATCH - edit a specific comment
+// ** Work in comment owner validation **
+router.patch("/projects/:projectId/:commentId", requireToken, removeBlanks, (req, res, next) => {
+    // Prevent client from changing comment owner
+    delete req.body.owner
+    const currentUser = req.user.id
+    const projectId = req.params.projectId
+    const commentId = req.params.commentId
+    const commentUpdate = req.body.comment.text
+
+    // ********get comment owner _id to compare
+
+    Project.updateOne({
+        "_id": ObjectId(projectId)
+    },{
+        $set: {
+            "comments.$[comments].text": commentUpdate
+        }
+    },{
+        "upsert": false,
+        "new": true,
+        arrayFilters: [
+            {
+                "comments._id": {
+                    "$eq": ObjectId(commentId)
+                }
+            }
+        ]
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+
+// REMOVE
+// Delete - delete comment
+router.delete("/sightings/:sightingId/:commentId", requireToken, (req, res, next) => {
+    const sightingId = req.params.sightingId
+    const commentId = req.params.commentId
+    Sighting.updateOne({
+        "_id": ObjectId(sightingId),
+        "comments": {
+            $elemMatch: { _id: ObjectId(commentId) }
+        }
+    },{
+        $pull: {
+            "comments": { "_id": commentId }
+        }
+    },{
+        new: true,
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
 })
 // 
 /////////////////////
