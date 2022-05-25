76ce3946c75a65264d15ade87ef0d1d65a0e678e
NikPurvis <nikpurvis@gmail.com>
2022-05-25 15:26:35 -0600

    Basic card layout for project page, now with details

--- ss-client/src/components/projects/IndexProjects.js
+++ ss-client/src/components/projects/IndexProjects.js
@@ -2,6 +2,14 @@
 
 import React, { useState, useEffect } from "react"
 import { getAllProjects } from "../../api/projects"
+import { Card } from "react-bootstrap"
+
+// Declare a style object to corral the Project cards
+const cardContainerLayout = {
+    display: "flex",
+    justifyContent: "center",
+    flexFlow: "row wrap"
+}
 
 const IndexProjects = (props) => {
     const [projects, setProjects] = useState(null)
@@ -20,22 +28,33 @@ const IndexProjects = (props) => {
         return <p>No projects, go add some!</p>
     }
 
-    let projectsJsx
+    let projectCards
 
     if (projects.length > 0) {
-        projectsJsx = projects.map(project => (
-            <li key={project.id}>
-                {project.name}
-            </li>
+        projectCards = projects.map(project => (
+            <Card key={project.id} style={{ width: "30%" }} className="m-2" >
+                <Card.Header>{project.name}</Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        {project.description}<br />
+                        {project.type}<br />
+                        {project.skill}<br />
+                        {project.notes}<br/>
+                        {project.owner.username}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         ))
+        
+        
     }
 
     return (
         <>
             <h3>All Projects</h3>
-            <ul>
-                {projectsJsx}
-            </ul>
+            <div style={cardContainerLayout}>
+                {projectCards}
+            </div>
         </>
     )
 }
